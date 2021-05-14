5c31772b37668734e64b037cbb1df3dd9fadb20a
Karla-Jaime <you@example.com>
2021-05-13 18:59:51 -0700

    Continuación MLAPI

--- Assets/Scripts/ConectionManager.cs
+++ Assets/Scripts/ConectionManager.cs
@@ -2,16 +2,42 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using MLAPI;
+using MLAPI.Spawning;
+using System;
+using Random=UnityEngine.Random;// Si sale error con random cambiarlo por: Random=System.Random
 
 public class ConectionManager : MonoBehaviour
 {
+    public GameObject ConnectionButtonPanel; 
     public void Host()
     {
-        NetworkManager.Singleton.StartHost();
+        ConnectionButtonPanel.SetActive(false);
+        NetworkManager.Singleton.ConnectionApprovalCallback += ApprovalCheck; 
+        NetworkManager.Singleton.StartHost(GetRandomSpawn(), Quaternion.identity);
+        
+    }
+
+    public void ApprovalCheck( byte[] connectionData , ulong clientID, NetworkManager.ConnectionApprovedDelegate callback)
+    {
+        //Debug.Log("Approving a connection");
+        bool approve = System.Text.Encoding.ASCII.GetString(connectionData) == "Password1234";
+        callback(true, null, approve, GetRandomSpawn(), Quaternion.identity);
     }
 
     public void Join()
     {
+        ConnectionButtonPanel.SetActive(false);
+        NetworkManager.Singleton.NetworkConfig.ConnectionData = System.Text.Encoding.ASCII.GetBytes("Password1234");
         NetworkManager.Singleton.StartClient();
     }
+
+    Vector3 GetRandomSpawn()
+    {
+        float x = Random.Range(-10f, 10f);
+        float y = Random.Range(-10f, 10f);
+        float z = Random.Range(-10f, 10f);
+
+        return new Vector3(x,y,z);
+    } 
+    
 }
